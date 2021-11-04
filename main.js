82295d29c6767789505c17b17c45d80fed82f466
ulldecorb <ausias.bertran@gmail.com>
2021-11-04 18:28:26 +0100

    Add hide-show articles functionality

--- main.js
+++ main.js
@@ -1,8 +1,32 @@
 const titleColor = document.getElementById('header__sub-title').style;
+const skills = document.getElementById('skills');
+const aboutMe = document.getElementById('about-me');
+const experience = document.getElementById('experience');
+const education = document.getElementById('education');
+const skillsBox = document.getElementById('skills__box').style;
+const aboutMeBox = document.getElementById('about-me__box').style;
+const experienceBox = document.getElementById('experience__box').style;
+const educationBox = document.getElementById('education__box').style;
 let counter = 0;
+
 setInterval(() => {
     counter%2 && (titleColor.color = '#f0f')
     !(counter%2) && (titleColor.color = '#000')
-    console.log(counter + ' ' + counter%2);
     counter ++;
 }, 800);
+
+aboutMe.addEventListener('click', () => {
+    aboutMeBox.display === 'flex' ? aboutMeBox.display = 'none' : aboutMeBox.display = 'flex';
+})
+
+experience.addEventListener('click', () => {
+    experienceBox.display === 'flex' ? experienceBox.display = 'none' : experienceBox.display = 'flex';
+})
+
+education.addEventListener('click', () => {
+    educationBox.display === 'flex' ? educationBox.display = 'none' : educationBox.display = 'flex';
+})
+
+skills.addEventListener('click', () => {
+    skillsBox.display === 'flex' ? skillsBox.display = 'none' : skillsBox.display = 'flex';
+})
